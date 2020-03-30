53851815826f81ddb6ce7746197883fe36c39dc1
VsMath3us <62157441+VsMath3us@users.noreply.github.com>
2020-03-30 12:25:14 -0300

    Pagina principal
    Foi adicionado o resto do conteudo

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<html lang= "pt-br"> 
-<head> 
-<meta chartset= "UTF-8"> 
-<title> Introdução ao GitHub </title> 
-</head
-<body> 
+< html  lang = " pt-br " > 
+< head > 
+< meta  chartset = " UTF-8 " > 
+< title > Introdução ao GitHub </ title > 
+</ head>
+< body > 
 <? php
-echo "New file in my Github repository"; 
+echo  "Novo arquivo no meu repositório Github" ;
 ?> 
-<div> 
-<h1> O que é o GitHub? </h1> 
-<P> É uma plataforma que permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribuam em projetos reservados ou Código-fonte aberto de qualquer lugar do mundo que usa hospedagem de código-fonte com controle de versão usando ou git </p> 
-</div>
-</body> 
-</html> 
+< div > 
+< h1 > O que é o GitHub? </ h1 > 
+< P > Uma plataforma que permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribua em projetos reservados ou Código-fonte aberta de qualquer lugar do mundo que usa hospedagem de código-fonte com controle de versão usando ou git </ p >
+<h2> Como criar um repositório</h2>
+<p>Para criar um repositório no Github você deve ir no simbolo de "+" no canto superior direito e ir na opção de "New repository", após isso escolha um nome para tal e se quiser uma breve descrição do que ele é, escolha se você quer que seu repositório seja "Público ou Privado" e se que se ele seja iniciado com um "Leiame" e por fim vá em criar repositório e ele estará pronto</p>
+<h3> Público</h3>
+<p>Qualquer um pode ver o repositório, e você pode escolher quem irá fazer os commts.</p>
+<h4> Privado</h4>
+<p>Você escolhe quem pode visualizar em quem pode fazer o commit.</p>
+<h5> Clone</h5>
+<p>Quando você crua um repositório, ele passa a existir como um repositório remote, é possível clonar o repositório para criar uma cópia local no seu computador e sincronizar entre os dois locais.</p>
+<h6> Inserção de Arquivos</h6>
+<p>Para a inserção de arquivos, vá até a pagina principal do repositório e clique em "Upload files", após arraste o arquivo ou pasta que deseja fazer na árvore de arquivos, na parte inferior digite uma mensagem de confirmação curta e significativa que mostre a alteração feita no arquivo, abaixo do campo da mensagem de confirmação optei por adicionar ou confirmar a ramificação atual ou uma nova ramificação e por último clique em "Commit changes" para fazer a commit das alterações.</p>
+</ div >
+</ body > 
+</ html > 
