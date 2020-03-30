787e5e50cfdeb2008412e6d7524f744f675a501e
VsMath3us <62157441+VsMath3us@users.noreply.github.com>
2020-03-30 01:31:25 -0300

    Criando Pagina Principal
    Foi inserido um novo arquivo

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang= "pt-br"> 
+<head> 
+<meta chartset= "UTF-8"> 
+<title> Introdução ao GitHub </title> 
+</head
+<body> 
+<? php
+echo "New file in my Github repository"; 
+?> 
+<div> 
+<h1> O que é o GitHub? </h1> 
+<P> É uma plataforma que permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribuam em projetos reservados ou Código-fonte aberto de qualquer lugar do mundo que usa hospedagem de código-fonte com controle de versão usando ou git </p> 
+</div>
+</body> 
+</html> 
